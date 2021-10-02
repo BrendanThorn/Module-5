9f18624f8e6b94677e118dac6e2244703e2431da
Brendan Thorn <Bre.thorn@outlook.com>
2021-10-02 16:28:42 -0700

    Loopy loops Home work

---
+++ Module 5/listy_lo.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+
+#list
+    food = ['rice', 'beans']
+
+#Appending list
+    food.append('broccoli')
+
+#Extending the list
+    added_food = ('bread', 'pizza')
+    food.extend(added_food)
+
+#printing the first two
+    print(food[0:2])
+
+#Printing the last item
+    print(food[-1])
+    
+#splitting breakfast
+    Breakfast = "eggs,fruit,orange juice"
+    Split_breakfast = Breakfast.split(",")
+    Breakfast = Split_breakfast 
+    print(Breakfast)
+
+#
+    len(Breakfast)
--- Module 5/loopy_loops.py
+++ Module 5/loopy_loops.py
@@ -1,4 +1,24 @@
 if __name__ == '__main__':
     pokemon =('picachu', 'charmander', 'bulbasaur')
+
     print(pokemon[1])
+
+    starter1 = pokemon[0]
+    starter2 = pokemon[1]
+    starter3 = pokemon[2]
+
+    #my_name = tuple('Brendan')
+    #'i' in my_name
+
+    #for N in range(2,11):
+     #   print(N)
     
+    M = 2
+    B = 11
+    #while M < B:
+    #    print(M)
+     #   M += 1
+
+    starter_string = 'This is a simple string'
+    for x in starter_string:
+        print(x)
