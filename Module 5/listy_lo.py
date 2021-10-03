7cbc9c9fea598e5bd58374620f93c54dd54e92d4
Brendan Thorn <Bre.thorn@outlook.com>
2021-10-02 19:07:34 -0700

    Update listy_lo.py

--- Module 5/listy_lo.py
+++ Module 5/listy_lo.py
@@ -28,11 +28,24 @@ if __name__ == '__main__':
 #Input fun
     number_list = []
     while True:
-        number_list_input = list(input("Enter number to be calculated. Enter stop to stop: ").strip())
+        number_list_input = input("Enter number to be calculated. Enter stop to stop: ")
         number_list.append(number_list_input)
         if ('s' in number_list_input):
-            print('exiting')
             number_list.pop()
             break
     print(number_list)
     
+    for x in range(0, len(number_list)):
+        number_list[x] = int(number_list[x])
+        
+    print(type(number_list[0]))
+    print(type(number_list[1]))
+    print(type(number_list[2]))
+
+
+    print(min(number_list))
+    print(max(number_list))
+    Num_sum = sum(number_list)
+    Len_sum = len(number_list)
+    print(Num_sum/Len_sum)
+
